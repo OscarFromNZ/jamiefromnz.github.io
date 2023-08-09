abd910b06051728d2c360dddafcf2eeba4cd9f70
Jamieee! <93412639+JamieFromNZ@users.noreply.github.com>
2023-08-09 13:17:47 +1200

    chat gptd it

--- SpeedBoost.js
+++ SpeedBoost.js
@@ -1,14 +1,12 @@
 class SpeedBoost extends Food {
     constructor(x, y, size) {
-        // Call the superclass constructor with the specific color for a speed boost
         super(x, y, size, 'green');
     }
 
-    // Override the applyEffect method to apply a speed boost to the player
-    eat(player) {
+    eat(player, game) {
         console.log('Speed boost eaten');
-        this.generateRandomFood();
+        game.generateRandomFood();
 
-        player.speed *= 2; // Double the player's speed
+        player.speed *= 2;
     }
 }
